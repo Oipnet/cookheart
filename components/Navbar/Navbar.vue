4c3faa9d306a706a13924d86eebf26309ff7aae2
defaut <test@mail.fr>
2020-03-20 14:53:36 +0100

    feat(navbar): Create components

---
+++ components/Navbar/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <navbar-logo />
+    <navbar-toggler @toggle-navbar="handleToggle" />
+    <navbar-menu />
+  </nav>
+</template>
+
+<script>
+import NavbarLogo from '~/components/Navbar/NavbarLogo'
+import NavbarToggler from '~/components/Navbar/NavbarToggler'
+import NavbarMenu from '~/components/Navbar/NavbarMenu'
+
+export default {
+  name: 'navbar',
+  components: {
+    NavbarLogo,
+    NavbarToggler,
+    NavbarMenu
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    handleToggle() {
+      this.open = !this.open
+    }
+  }
+}
+</script>
---
+++ components/Navbar/NavbarMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    :class="open ? 'block' : 'hidden'"
+    class="w-full block flex-grow lg:flex lg:items-center lg:w-auto"
+  >
+    <div class="text-sm lg:flex-grow">
+      <navbar-menu-item
+        v-for="(item, index) in items"
+        :key="index"
+        :item="item"
+      />
+    </div>
+    <div>
+      <a
+        href="#"
+        class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        >Download</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import jsonMenu from '~/components/Navbar/menu.json'
+import NavbarMenuItem from '~/components/Navbar/NavbarMenuItem'
+
+export default {
+  name: 'navbar-menu',
+  components: { NavbarMenuItem },
+  data() {
+    return { items: [] }
+  },
+  mounted() {
+    this.items = jsonMenu
+  }
+}
+</script>
